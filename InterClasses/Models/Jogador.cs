f434afa83874050537a3765f7dd985c0eee79b28
EnzoNicoletti <enzo.nicoletti@aluno.senai.br>
2025-08-21 15:22:48 -0300

    Adicionar arquivos de projeto.

---
+++ InterClasses/Models/Jogador.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterClasses.Models
+{
+    public class Jogador
+    {
+        public int JogadorId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome do Jogador é Obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Nome do Jogador pode ter, no máximo, 50 caracteres.")]
+        [Display(Name = "Nome do Jogador")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Número do Jogador é Obrigatório")]
+        [Range(1, 99, ErrorMessage = "O Número do Jogador deve estar entre 1 e 99.")]
+        [Display(Name = "Número")]
+        public int Numero { get; set; }
+
+        [Display(Name = "Posição")]
+        [MaxLength(30, ErrorMessage = "A Posição do Jogador pode ter, no máximo, 30 caracteres.")]
+        public string? Posicao { get; set; }
+
+        // Relacionamento com a Tabela Time
+        [Display(Name = "Time")]
+        public int TimeId { get; set; } // Vincular o campo do Id da tabela
+        public Time? Time { get; set; } // Vincular o Ojeto relacionado ao Id acima
+    }
+}
---
+++ InterClasses/Models/Time.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace InterClasses.Models
+{
+    public class Time
+    {
+
+        public int TimeId { get; set; }
+
+        // Torna o campo "Nome" Obrigatório
+        [Required(ErrorMessage = "O campo Nome do Time é Obrigatório")]
+        [MaxLength(50, ErrorMessage = "O Nome do Time pode ter, no máximo, 50 caracteres.")]
+        [Display(Name = "Nome do Time")]
+        public string? Nome { get; set; }
+
+        // Relacionamento entre Models (Tableas no Banco de dados)
+        // Chave Estrangeira
+        [Display(Name = "Esporte")]
+        public int EsporteId { get; set; } // Vincular o campo do Id da tabela 
+        public Esporte? Esporte { get; set; } // Vincular o Objeto relacionado ao Id acima
+    }
+}
